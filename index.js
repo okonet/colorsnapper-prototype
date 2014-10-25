86a993a8736d2e47f6940d467b3a3d4f842c3b05
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2014-10-25 14:20:11 +0200

    Changed port of the connect server to fit heroku routing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var connect = require('connect')
 var serveStatic = require('serve-static')
+var port = Number(process.env.PORT || 5000);
 
 var app = connect()
 
 app.use(serveStatic('.', {'index': ['index.html']}))
 app.use(serveStatic('./build'))
-app.listen(3000)
+app.listen(port)
