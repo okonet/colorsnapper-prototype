969568de64f69b3a4bc78f256a9b7a5c69bcaac9
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-08-31 21:40:54 +0200

    Removed redundant heroku stuff

--- index.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect')
-var serveStatic = require('serve-static')
-var port = Number(process.env.PORT || 5000);
-
-var app = connect()
-
-app.use(serveStatic('.', {'index': ['index.html']}))
-app.use(serveStatic('./build'))
-app.listen(port)
